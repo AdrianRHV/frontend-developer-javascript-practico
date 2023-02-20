f2878325fd5ac2f90a8cc5b3d8c1d3dce4703195
AdrianRHV <rascuas_29998@hotmail.com>
2023-02-19 22:47:50 -0600

    se agrego menu mobile, se arreglaron bug de margenes y paddings, se agrego condicionales de despegables, tambien se ajusto pantalla en modo mobile.

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
 const menuEmail = document.querySelector('.nav-bar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+const menuCarritoIcon = document.querySelector('.nav-bar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleShoppingCartMenu);
 
 function toggleDesktopMenu() {
+  console.log('Click');//debugg
   desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu() {
+  console.log('Click');//debugg
+  const isAsideClosed = aside.classList.contains('inactive');
+
+  if (!isAsideClosed) {
+    aside.classList.add('inactive')
+    } 
+  mobileMenu.classList.toggle('inactive');
+}
+function toggleShoppingCartMenu() {
+  console.log('Click');//debugg
+
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive')
+    } 
+  
+    aside.classList.toggle('inactive');
+}
+
+
+
+console.log('JS funcionando');//debugg
