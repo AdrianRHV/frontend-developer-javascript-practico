7461de78fad525807b95047591f9027b0247b949
AdrianRHV <rascuas_29998@hotmail.com>
2023-02-27 23:18:38 -0600

    se agregaron las paginas de carrito y estilos.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const mobileMenu = document.querySelector('.mobile-menu');
 
 const menuCarritoIcon = document.querySelector('.nav-bar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -45,6 +46,66 @@ function toggleShoppingCartMenu() {
     aside.classList.toggle('inactive');
 }
 
+//lista de productos
+const productList =[];
+productList.push({
+  name: 'Bike',
+  price: 120,
+  image: 'https://images.squarespace-cdn.com/content/v1/535d346fe4b03ecd54f1fd25/1649361530216-82XYXK0EY7301TMSEAS5/Sklar+Bikes+Super+Something+Steel+Gravel+Bike780.jpg?format=1500w',
+})
+productList.push({
+  name: 'Pantalla',
+  price: 400,
+  image: 'https://m.media-amazon.com/images/I/61nwkFpUBLL._AC_SS450_.jpg',
+})
+productList.push({
+  name: 'Computadora',
+  price: 1000,
+  image: 'https://sistemasyprogramas.com/8379-medium_default/computador-intel-dual-core-160ghz-4gb-ddr3-1tb-hdd-16-led.jpg',
+})
+
+
+function renderProducts(arr) {
+  for (product of arr){
+    const productCard = document.createElement ('div');
+    productCard.classList.add('product-card');
+  
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+    //product = {name, price, image} -> product.image.
+  
+    const productInfo = document.createElement ('div');
+    productInfo.classList.add('info-product');
+  
+    const productInfoDiv = document.createElement ('div');
+  
+    const productInfoPrice = document.createElement ('p');
+    productInfoPrice.innerText = '$' + product.price;
+    const productInfoName = document.createElement ('p');
+    productInfoName.innerText = product.name;
+  
+    productInfoDiv.appendChild(productInfoPrice);
+    productInfoDiv.appendChild(productInfoName);
+  
+    const productInfoFigure = document.createElement ('figure');
+    const productInfoImgCart = document.createElement ('img');
+    productInfoImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+    productInfoFigure.appendChild(productInfoImgCart);
+  
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+  
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+  
+    cardsContainer.appendChild(productCard);
+  
+    console.log(productImg);
+  }
+}
+
+renderProducts(productList);
 
 
 console.log('JS funcionando');//debugg
